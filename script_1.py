db236b040c85bf4e58b36c60a8e06288eeb5eb98
Mikella C <mikella.c@outlook.com>
2023-03-18 18:08:08 -0400

    Create script_1.py

---
+++ script_1.py
@@ -0,0 +1,11 @@
+def main ():
+    print("The main function is running.")
+    print("The _name_ parameter is: " +_name_)
+
+
+if _name_ == "_main_":
+    main ()
+else:
+    print("The main function is not running.")
+    print ("The _name_ parameter is: " +_name_)
+    
